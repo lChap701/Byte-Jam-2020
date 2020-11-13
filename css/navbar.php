7334155479d22c671eba12773ef41802d7fed0b2
lChap701 <lucas.chapman@stu.indianhills.edu>
2020-11-13 11:05:47 -0600

    Stylesheet and navbar

---
+++ css/navbar.php
@@ -0,0 +1,17 @@
+<nav>
+    <div id="subnav-container">
+        <div class="subnav">
+            <a class="subnav-link" href="logout.html" title="Logout">Logout</a>
+            <div class="subnav-content">
+                <a href="account.php" title="Account">Account</a>
+                <a href="delete.php" title="Delete Account">Delete Account</a>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <a href="index.php" title="Home">Home</a>
+        <a href="map.php" title="Map">Map</a>
+        <a href="facts.php" title="Facts">Facts</a>
+    </div>
+</nav>

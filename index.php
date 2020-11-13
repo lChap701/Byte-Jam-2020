ebb6e7c6e3799ce58d3f3adc704634e5e4783230
lChap701 <lucas.chapman@stu.indianhills.edu>
2020-11-13 11:03:42 -0600

    Home page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <?php include "css/navbar.php"?>
+    <div>
+
+        <div id="wrapper" class="special">
+            <div id="container" class="special">
+                <h1>Welcome!!!</h1>
+            </div>
+        </div>
+</body>
+
+</html>
